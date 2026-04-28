decbb4267b825c161cdb19cdc1ec0967380f6aee
imamhossainsrabon <imamhossainsrabon@gmail.com>
2026-04-28 21:28:31 +0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const outfitSans = Outfit({
+  subsets:["latin"]
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +26,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${outfitSans.className} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="max-w-7xl mx-auto">
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
